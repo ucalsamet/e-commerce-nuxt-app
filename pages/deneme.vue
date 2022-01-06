0843589525e632f0b7b4c5229d56da2d18fe7e34
= <=>
2022-01-06 05:09:47 +0300

    init

---
+++ pages/deneme.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+   <ul>
+     <li v-for="c in getC.countCart" :key="c._id">
+
+       {{c.title}}
+     </li>
+
+     
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+computed:{
+  getUserId(){
+    return this.$store.getters.getUser
+  },
+    getC(){
+        return this.$store.getters.getUserCheck.find(item => {
+          return item.userId == this.getUserId.userId
+        })
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ store/cart/index.js
@@ -0,0 +1,32 @@
+export default {
+  state: {
+    userCheck: [],
+  },
+  mutations: {
+    setUserCheck(state, payload) {
+      state.userCheck = payload;
+    },
+    addUserCheck(state, payload) {
+      state.userCheck.push(payload);
+    }
+  },
+  actions: {
+    nuxtServerInit(vuexContext, context) {
+      return context.$axios.get("/get-usercheck").then(response => {
+        vuexContext.commit("setUserCheck", response.data.data);
+        console.log(response.data.data);
+      });
+    },
+    addUserCheck(vuexContext, payload) {
+      this.$axios.post("/add-usercheck", payload).then(response => {
+        console.log(response.data.data);
+        vuexContext.commit("addUserCheck", response.data.data);
+       });
+    },
+  },
+  getters: {
+    getUserCheck(state) {
+      return state.userCheck
+    }
+  }
+};
